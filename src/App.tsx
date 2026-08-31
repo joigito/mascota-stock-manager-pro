f64894ae7064502ad7d9e43b0c31b50eac34761a
OrionCat <jorgedigitall@gmail.com>
2026-08-31 22:35:21 +0000

    chore(cleanup): remove dead FeatureFlagsManager and unused react-query wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { OrganizationProvider } from "@/contexts/OrganizationProvider";
 import Index from "./pages/Index";
@@ -12,35 +11,31 @@ import NotFound from "./pages/NotFound";
 import { AcceptInvitation } from "./pages/AcceptInvitation";
 import PublicAccountStatement from "./pages/PublicAccountStatement";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <OrganizationProvider>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/accept-invitation" element={<AcceptInvitation />} />
-            <Route path="/tienda/:slug/cuenta/:token" element={<PublicAccountStatement />} />
-            <Route path="/tienda/:slug" element={
-              <ProtectedRoute>
-                <Store />
-              </ProtectedRoute>
-            } />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Index />
-              </ProtectedRoute>
-            } />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </OrganizationProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+      <OrganizationProvider>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/accept-invitation" element={<AcceptInvitation />} />
+          <Route path="/tienda/:slug/cuenta/:token" element={<PublicAccountStatement />} />
+          <Route path="/tienda/:slug" element={
+            <ProtectedRoute>
+              <Store />
+            </ProtectedRoute>
+          } />
+          <Route path="/" element={
+            <ProtectedRoute>
+              <Index />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </OrganizationProvider>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export { App };
